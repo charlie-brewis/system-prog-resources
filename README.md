ad1925b945ab6bf7c39258bccf4e6b92946279bd
Charlie Brewis <charliebrewis7@gmail.com>
2024-04-08 17:55:03 +0100

    FINALLY WORKING BOOT + software included in readme.md

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # system-prog-resources
+
 A repository containing all my resources and files which i am using to learn systems programming
 
-
 At first i am using [this](https://www.youtube.com/watch?v=MwPjvJ9ulSc&list=PLm3B56ql_akNcvH8vvJRYOc7TbYhRs19M&index=1) playlist as a guide
+
+___
+## Software used:
+Assembler - NASM version 2.16.02
+Virtual Machine - Qemu
--- boot.asm
+++ boot.asm
@@ -1,16 +1,23 @@
 
+; Assembler Command:
+;       nasm -f bin boot.asm -o boot.bin
+; VM Command:
+;       qemu-system-x86_64 boot.bin
+
+
+
 ; Jumps to the current memory address - i.e., The boot will infinately loop
 jmp $
 
-/* 
-A boot sector is always bytes (i.e., assembly lines) long. 
 
-Therefore this command 'define byte 0's the exact number of times needed to get to 510 lines 
-- (i.e., makes it so that every blank line is actually just 0 up to 510 lines).
+; A boot sector is always bytes (i.e., assembly lines) long. 
 
-This is done as $ = current address, and $$ = section start address,
-therefore $ - $$ = the number of lines of code before this one.
-*/
+; Therefore this command 'define byte 0's the exact number of times needed to get to 510 lines 
+; - (i.e., makes it so that every blank line is actually just 0 up to 510 lines).
+
+; This is done as $ = current address, and $$ = section start address,
+; therefore $ - $$ = the number of lines of code before this one.
+
 times 510-($-$$) db 0
 ; The last 2 lines in a boot sector are always 0x55 0xaa, therefore we db them at the end.
 ; 0x is simply a prefix, stating that the following number is in hexadecimal
