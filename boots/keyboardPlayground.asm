c303f37d3dad945cd4cfe5c5c96b02ff9e6bc818
Charlie Brewis <charliebrewis7@gmail.com>
2024-04-10 19:46:51 +0100

    added enter functionality to keyboardPlayground.asm

--- boots/keyboardPlayground.asm
+++ boots/keyboardPlayground.asm
@@ -4,22 +4,30 @@
 [org 0x7c00] ; Fix pointer offset
 
 ;* Typing Exercise
-; typingLoop:
-;     mov ah, 0x00
-;     int 0x16
-;     mov ah, 0x0e
-;     int 0x10
-;     cmp al, 8
-;     je backspacePressed
-;     endBackspacePressed:
-;     jmp typingLoop
+typingLoop:
+    mov ah, 0x00
+    int 0x16
+    mov ah, 0x0e
+    int 0x10
+    cmp al, 8
+    je backspacePressed
+    endBackspacePressed:
+    cmp al, 13
+    je enterPressed
+    endEnterPressed:
+    jmp typingLoop
 
-; backspacePressed:
-;     mov al, 32 ; Space
-;     int 0x10
-;     mov al, 8 ; Another backspace
-;     int 0x10
-;     jmp endBackspacePressed
+backspacePressed:
+    mov al, 32 ; Space
+    int 0x10
+    mov al, 8 ; Another backspace
+    int 0x10
+    jmp endBackspacePressed
+
+enterPressed:
+    mov al, 10
+    int 0x10
+    jmp endEnterPressed
 
 
 
@@ -40,33 +48,33 @@
 
 
 ;* Store string exercise
-buffer:
-    times 10 db 0
+; buffer:
+;     times 10 db 0
 
-mov bx, buffer
+; mov bx, buffer
 
-inputString:
-    mov ah, 0x00
-    int 0x16
-    cmp al, 13
-    je setupPrint 
-    mov [bx], al 
-    inc bx
-    jmp inputString
+; inputString:
+;     mov ah, 0x00
+;     int 0x16
+;     cmp al, 13
+;     je setupPrint 
+;     mov [bx], al 
+;     inc bx
+;     jmp inputString
 
-setupPrint:
-    mov bx, buffer 
-    mov ah, 0x0e
+; setupPrint:
+;     mov bx, buffer 
+;     mov ah, 0x0e
 
-printString:
-    mov al, [bx]
-    cmp al, 0
-    je end
-    int 0x10
-    inc bx
-    jmp printString
+; printString:
+;     mov al, [bx]
+;     cmp al, 0
+;     je end
+;     int 0x10
+;     inc bx
+;     jmp printString
 
-end:
+; end:
 
 
 
