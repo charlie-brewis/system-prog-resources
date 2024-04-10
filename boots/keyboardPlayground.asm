1efcdff223d2ed0bec5505a9ffa24d3c7b34c085
Charlie Brewis <charliebrewis7@gmail.com>
2024-04-10 16:44:10 +0100

    Storing strings done

--- boots/keyboardPlayground.asm
+++ boots/keyboardPlayground.asm
@@ -3,24 +3,6 @@
 
 [org 0x7c00] ; Fix pointer offset
 
-;* Saving an input char to a variable
-; Define a varaible of a single byte, initialised with 0
-; char:
-;     db 0
-
-; mov ah, 0x00
-; int 0x16
-; mov [char], al ; Set's char's value to the value of al
-
-;* Saving an input string to memory
-; stringBuffer:
-;     times 10 db 0 ; Initialise a space in memory of 10 empty bytes for the string
-;     mov bx, stringBuffer ; set bx to a pointer to the beggining of the buffer
-;     mov [bx], al ; Save the input string to the location of the pointer to the buffer 
-;     inc bx ; Increment buffer pointer
-;     jmp inputLoop
-
-
 ;* Typing Exercise
 ; typingLoop:
 ;     mov ah, 0x00
@@ -40,15 +22,49 @@
 ;     jmp endBackspacePressed
 
 
-mov ah, 0x00
-int 0x16
-mov [char], al
-printChar:
-    mov al, [char]
+
+;* Store char exercise
+; mov ah, 0x00
+; int 0x16
+; mov [char], al
+; printChar:
+;     mov al, [char]
+;     mov ah, 0x0e
+;     int 0x10
+; char:
+;     db 0
+
+
+;* Store string exercise
+buffer:
+    times 10 db 0
+
+mov bx, buffer
+
+inputString:
+    mov ah, 0x00
+    int 0x16
+    cmp al, 13
+    je setupPrint 
+    mov [bx], al 
+    inc bx
+    jmp inputString
+
+setupPrint:
+    mov bx, buffer 
     mov ah, 0x0e
+
+printString:
+    mov al, [bx]
+    cmp al, 0
+    je end
     int 0x10
-char:
-    db 0
+    inc bx
+    jmp printString
+
+end:
+
+
 
 jmp $ ; Infinate loop at end of boot
 times 510 - ($ - $$) db 0 ; define 0 as many times as needed to fill boot sector (512B)
