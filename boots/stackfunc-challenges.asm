a547d370b087182ff266aca0d5fe3664d835df2a
Charlie Brewis <charliebrewis7@gmail.com>
2024-04-14 19:53:57 +0100

    Stack challenge working but printing backwards

--- boots/stackfunc-challenges.asm
+++ boots/stackfunc-challenges.asm
@@ -4,17 +4,31 @@ setupStack:
     mov bp, 0x800
     mov sp, bp 
 
-getInput:
-    mov ah, 0x00
-    int 0x16
 
-pushInputToStack:
-    push ax 
+inputLoop:
+    getInput:
+        mov ah, 0x00    ; Need to reset as after int 0x16 ah is set to the scan code of the input char
+        int 0x16
+
+    pushInputToStack:
+        push ax
+
+    checkIfEnter:
+        cmp al, 13
+        je setupPrint
+    jmp inputLoop
+
+
+setupPrint:
+    mov ah, 0x0e
 
 printStack:
     pop ax
     mov ah, 0x0e
     int 0x10
+    cmp bp, sp
+    jne printStack
+
 
 
 
