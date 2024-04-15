c959e354e8ab2a9651fce63b01f4c9df79683b51
Charlie Brewis <charliebrewis7@gmail.com>
2024-04-15 11:12:29 +0100

    function challenge complete

--- boots/stackfunc-challenges.asm
+++ boots/stackfunc-challenges.asm
@@ -1,50 +1,64 @@
 
 ;* Stack challenge
-setupStack:
-    mov bp, 0x800
-    mov sp, bp 
+; setupStack:
+;     mov bp, 0x800
+;     mov sp, bp 
 
 
-inputLoop:
-    getInput:
-        mov ah, 0x00    ; Need to reset as after int 0x16 ah is set to the scan code of the input char
-        int 0x16
+; inputLoop:
+;     getInput:
+;         mov ah, 0x00    ; Need to reset as after int 0x16 ah is set to the scan code of the input char
+;         int 0x16
 
-    pushInputToStack:
-        push ax
+;     pushInputToStack:
+;         push ax
 
-    checkIfEnter:
-        cmp al, 13
-        je setupPrint
-    jmp inputLoop
+;     checkIfEnter:
+;         cmp al, 13
+;         je setupPrint
+;     jmp inputLoop
 
 
-setupPrint:
-    mov ah, 0x0e
-
-; printStackBackwards:
-;     pop ax
+; setupPrint:
 ;     mov ah, 0x0e
+
+; ; printStackBackwards:
+; ;     pop ax
+; ;     mov ah, 0x0e
+; ;     int 0x10
+; ;     cmp bp, sp
+; ;     jne printStackBackwards
+
+; printStackForward:
+;     decrementStackBase:
+;         sub bp, 2   ; Need to sub first as the base (0x800) is empty
+;     printValueAtStackBase:
+;         mov al, [bp]
+;         int 0x10
+;     exitIfStackEmpty:
+;         cmp bp, sp
+;         jne printStackForward
+
+; newLine:
+;     mov al, 10
 ;     int 0x10
-;     cmp bp, sp
-;     jne printStackBackwards
 
-printStackForward:
-    decrementStackBase:
-        sub bp, 2   ; Need to sub first as the base (0x800) is empty
-    printValueAtStackBase:
-        mov al, [bp]
+
+;* Functions Challenge
+
+call printBase10Integers
+jmp end
+
+printBase10Integers:
+    mov ah, 0x0e
+    mov al, '0'
+    intLoop:
         int 0x10
-    exitIfStackEmpty:
-        cmp bp, sp
-        jne printStackForward
-
-newLine:
-    mov al, 10
-    int 0x10
-  
-
-
+        inc al 
+        cmp al, '9' + 1
+        jne intLoop
+    ret
+end:
 
 
 jmp $ 
