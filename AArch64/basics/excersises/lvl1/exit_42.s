7ea89063ea2d2f42e69c80ad21d29699316c50b2
charlie-brewis <charliebrewis7@gmail.com>
2025-04-23 22:11:47 +0100

    ex 2 done

--- AArch64/basics/excersises/lvl1/exit_42.s
+++ AArch64/basics/excersises/lvl1/exit_42.s
@@ -4,3 +4,10 @@ Write a program that returns 42 as exit code
 Use mov x0, #42 before the exit syscall.
 Then check `echo $?` in the shell after running it
 */
+
+.section .text 
+.global _start
+_start:
+    mov x0, #42  // exit code = 42
+    mov x8, #93  // syscall: exit
+    svc #0
