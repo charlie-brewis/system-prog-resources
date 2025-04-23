f7aca1489280ddb20891387108240ee12816ce59
charlie-brewis <charliebrewis7@gmail.com>
2025-04-23 21:50:43 +0100

    added assembling section

--- AArch64/basics/aarch64_basics.s
+++ AArch64/basics/aarch64_basics.s
@@ -136,6 +136,29 @@ my_function:
 
 
 
+// Assembling, Linking, and Executing
+// Assemble to assembly file (.s) to object file (.o)
+//     `aarch64-linux-gnu-as -o hello_arm64.o hello_arm64.s`
+
+// Statically link the object file (.o) to create an executable
+//     `aarch64-linux-gnu-ld -static -o hello_arm64 hello_arm64.o`
+//         - Here we can statically link (and thus use QEMU) as there are no shared (dynamic) libaries
+
+// Run the executable using QEMU
+//     `qemu-aarch64 ./hello_arm64`
+
+
+
+/// More AArch64 syscall numbers (0-291) can be found here: 
+///   https://chromium.googlesource.com/chromiumos/docs/+/master/constants/syscalls.md#arm64-64_bit
+
+
+// Also note we can combine assembling and linking into a single command in gcc, just like with c code
+//     `aarch64-linux-gnu-gcc -static -nostartfiles -o hello_again hello_arm64.s`
+//         - We need to specify `-nostartfiles` as gcc expects a .c file which expects a `main` function
+
+
+
 //-------------------------------------------------------------------------------------------------
 // A Clarification on Moving immediate values
 
